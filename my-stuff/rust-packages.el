b729e27055bc6b288862719076a47360838580af
dario facchinetti <dario.fad@gmail.com>
2022-02-13 13:10:18 +0100

    enable rust auto fmt

--- my-stuff/rust-packages.el
+++ my-stuff/rust-packages.el
@@ -1,5 +1,11 @@
+;; editing rust files
 (use-package rust-mode
-  :ensure t)
+  :ensure t
+  :config
+
+  ;; enable auto formatting on save
+  (setq rust-format-on-save t)
+  )
 
 ;; start lsp when in rust mode
 (add-hook 'rust-mode-hook #'lsp-deferred)
